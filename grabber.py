9491b91733cea95b2a058f2c70add9aa9e1ee975
denn9s <cs.dennisong@gmail.com>
2020-06-18 20:18:53 -0700

    Added sample/test link

--- grabber.py
+++ grabber.py
@@ -1,11 +1,14 @@
 from prawInfo import reddit
 
+testLink = 'https://old.reddit.com/r/nba/comments/c0ky2l/board_man_celebrates/'
+
 def inputLink(link):
 	submission = reddit.submission(url = link)
 	return submission
 
 def main():
-	submission = inputLink('')
+	submission = inputLink(testLink)
+	print(submission.title)
 
 if __name__ == '__main__':
 	main()
